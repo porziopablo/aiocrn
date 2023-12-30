6b18d3d80d1466c0bd7691bdc561db021ba82fa4
pabloporzio <pabloporzio97@gmail.com>
2023-12-30 15:51:09 -0300

    eslint husky

--- App.tsx
+++ App.tsx
@@ -6,6 +6,8 @@ import { NavigationContainer } from '@react-navigation/native';
 // components
 import TabNavigator from './src/navigation';
 
+console.log('App.tsx');
+
 function App(): JSX.Element {
   return (
     <SafeAreaView>
